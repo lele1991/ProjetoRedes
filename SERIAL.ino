15e2acd4504f212ae311d66c1d69d67e9c1ed959
Letícia de Oliveira Nunes <38983849+lele1991@users.noreply.github.com>
2020-09-16 08:02:13 -0300

    Add files via upload

--- SERIAL.ino
+++ SERIAL.ino
@@ -1,3 +1,10 @@
+#include "DHT.h"      
+
+#define DHTPIN A1                         //DHT pino
+#define DHTTYPE DHT11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
 uint8_t end_com = 0;
 uint32_t inputString[11];
 int countInput = 0;
@@ -8,40 +15,39 @@ unsigned int checksum_calc = 0;
 typedef struct {
   uint32_t n_req;
   byte  cmd;
-  unsigned long  data;
+   uint32_t   data;
   unsigned int checksum;
 } COM;
 
-COM frame;
+
+uint32_t temperatura () {
+   float temp = 28.9; 
+//   float temp = dht.readTemperature();
+  uint32_t *temperatura = (uint32_t *) &temp; 
+  return  *temperatura;
+  
+}
+
+uint32_t umidade () {
+  float umi = 20.5; 
+//   float umi = dht.readHumidity(); 
+  uint32_t *umidade = (uint32_t *) &umi; 
+  return  *umidade;
+}
 
 void setup() {
   Serial.begin(9600);
+//  dht.begin();
 }
 
-///* Função que calcula o checksum */
-//unsigned int check_sum(  ) {
-//  unsigned int sum = 0;
-//  for (int j = 0; j < 9; j++) {
-//    sum += inputString[j];
-//  }
-//  //  Serial.println(~sum, HEX);
-//  return ~sum;
-//}
-
-unsigned long temperatura () {
-  unsigned long temp = 0x00003010;
-  return temp;
-}
-
-unsigned long umidade () {
-  unsigned long umi = 0x00003011;
-  return umi;
-}
 
 void loop() {
+  
+  COM frame;
+  
   if (stringComplete) {
     unsigned long nreq_reciv = 0;
-//    byte cdm_ = 0;
+    //    byte cdm_ = 0;
     unsigned long data_reciv = 0;
     unsigned int checksum_reciv = 0;
 
@@ -50,22 +56,22 @@ void loop() {
     unsigned long frame_3 = (unsigned long)(inputString[2] );
     unsigned long frame_4 = (unsigned long)(inputString[3] );
     nreq_reciv = (frame_1 << 24) | (frame_2 << 16) | (frame_3 << 8) | frame_4;
-    
+
     byte frame_5 = (byte)inputString[4] ;
-    
+
     unsigned long frame_6 = (unsigned long)(inputString[5] );
     unsigned long frame_7 = (unsigned long)(inputString[6] );
     unsigned long frame_8 = (unsigned long)(inputString[7] );
     unsigned long frame_9 = (unsigned long)(inputString[8] );
     data_reciv = (frame_6 << 24) | (frame_7 << 16) | (frame_8 << 8) | frame_9;
-    
+
     unsigned int frame_10 = (unsigned int)(inputString[9] );
     unsigned int frame_11 = (unsigned int)(inputString[10] );
 
-     
+
     checksum_reciv = (frame_10 << 8) | frame_11;
     checksum_calc = ~(nreq_reciv + frame_5 + data_reciv);
-//    Serial.println(checksum_calc, HEX);
+    //    Serial.println(checksum_calc, HEX);
     if (checksum_calc == checksum_reciv) {
 
       frame.n_req = nreq_reciv;
@@ -84,13 +90,18 @@ void loop() {
 
       sum = frame.n_req + frame.data + frame.cmd;
       frame.checksum = ~sum;
+      //
+//            Serial.println(frame.n_req, HEX);
+//            Serial.println(frame.cmd, HEX);
+//            Serial.println(frame.data, HEX);
+//            Serial.println(frame.checksum, HEX);
 
-//      Serial.write(0x4e);
+      //      Serial.write(0x4e);
 
-      Serial.write((const uint8_t*)&frame, sizeof(COM));
-//      Serial.write(frame.cmd);
-//      Serial.write(frame.data);
-//      Serial.write(frame.checksum);
+            Serial.write((const uint8_t*)&frame, sizeof(COM));
+      //      Serial.write(frame.cmd);
+      //      Serial.write(frame.data);
+      //      Serial.write(frame.checksum);
 
     } else {
       Serial.println("Checksum Errado");
@@ -106,7 +117,7 @@ void loop() {
 void serialEvent() {
   while (Serial.available()) {
     end_com = (uint8_t)Serial.read();
-//    Serial.println(end_com, HEX);
+//        Serial.println(end_com, HEX);
     if (end_com == 0x04) {
       stringComplete = true;
     } else {
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import time
 # Dados da comunicação
 DEVICE = '/dev/ttyUSB0'
 BAUD = 9600
+# RECIVED_DATA = 'd8a56d3cff3333e741f525'
+arduino = serial.Serial(DEVICE, BAUD)
 
 
 @dataclass
@@ -45,66 +47,90 @@ def split_frame(data):
     return data_splited
 
 
-def com_serial():
+def ordered_data(recived_data):
+    orderless_data = recived_data.strip('')
+    n = 2
+    orderless_data = [orderless_data[i:i+n]
+                      for i in range(0, len(orderless_data), n)]
 
-    arduino = serial.Serial(DEVICE, BAUD)
+    n_req_order = ''.join((orderless_data[:4])[::-1])
+    cmd_order = ''.join((orderless_data[4:5])[::-1])
+    data_order = ''.join((orderless_data[5:9])[::-1])
+    check_order = ''.join((orderless_data[9:11])[::-1])
+
+    # print(int(hex(int(data_order, 16)), 0))
+
+    n_req_order = hex(int(n_req_order, 16))
+    cmd_order = struct.pack('>B', int(cmd_order, 16))
+    data_order = struct.pack('>I', int(data_order, 16))
+    check_order = struct.pack('>H', int(check_order, 16))
+
+    # print(data_order)
+
+    return n_req_order, cmd_order, data_order, check_order
+
+
+def com_serial():
     arduino.flushInput()
     arduino.flushOutput()
     time.sleep(2)
+    print('Ho')
+    # while True:
+    # N_REQ - 4 bytes
+    arduino.write(frame.n_req)
+    time.sleep(1)
 
-    while True:
-        # N_REQ - 4 bytes
-        arduino.write(frame.n_req)
-        # time.sleep(1)
-        # arduino.write(n_req_tx[1].encode())
-        # time.sleep(1)
-        # arduino.write(n_req_tx[2].encode())
-        # time.sleep(1)
-        # arduino.write(n_req_tx[3].encode())
-        time.sleep(1)
+    # cmd - 1 byte (54 ou 48)
+    arduino.write(frame.cmd)
+    time.sleep(1)
 
-        # cmd - 1 byte (54 ou 48)
-        arduino.write(frame.cmd)
-        time.sleep(1)
+    # data - 4 bytes
+    arduino.write(frame.data)
+    time.sleep(1)
 
-        # data - 4 bytes
-        arduino.write(frame.data)
-        time.sleep(1)
-        # arduino.write(data_tx[1].encode())
-        # time.sleep(1)
-        # arduino.write(data_tx[2].encode())
-        # time.sleep(1)
-        # arduino.write(n_req_tx[3].encode())
-        # time.sleep(1)
+    # checksum - 2 bytes
+    arduino.write(frame.checksum)
+    time.sleep(1)
+    # time.sleep(1)
 
-        # checksum - 2 bytes
-        arduino.write(frame.checksum)
-        time.sleep(1)
-        # arduino.write(checksum_tx[1].encode())
-        # time.sleep(1)
+    # endframe
+    arduino.write(b'\x04')
+    time.sleep(2)
 
-        # endframe
-        arduino.write(b'\x04')
-        time.sleep(2)
-
-        inm = arduino.read(11).hex()
-        # inm = arduino.readline()
-        print(inm)
-        inm = 0
-        time.sleep(2)
+    RECIVED_DATA = arduino.read(11).hex()
+    # RECIVED_DATA = arduino.readline()
+    print(RECIVED_DATA)
+    # RECIVED_DATA = 0
+    time.sleep(2)
+    return RECIVED_DATA
 
 
 if __name__ == "__main__":
     n_req_tx = hex(random_number())
-    cmd_tx = struct.pack('B', 84)
+    cmd_tx = struct.pack('B', 72)
     data_tx = struct.pack('I', 0)
+
     checksum_tx = checksum_calc(n_req_tx, cmd_tx, data_tx)
 
     n_req_tx = binascii.unhexlify(n_req_tx.strip('0x'))
+
     checksum_tx = binascii.unhexlify(checksum_tx.strip('0x'))
     frame = Data_tx(n_req_tx, cmd_tx, data_tx, checksum_tx)
-
     print(frame)
-    # print(checksum_tx)
+    # com_serial()
+    while True:
+        print(frame)
+        recived_data = com_serial()
+        n_req_order, cmd_order, data_order, check_order = ordered_data(
+            recived_data)
 
-    com_serial()
+        # print(checksum_calc(n_req_order, cmd_order, data_order))
+        print(frame.cmd)
+        checksum_recived = True
+        if (checksum_recived):
+            if (frame.cmd == b'\x54'):
+                print("Temperatura: ")
+                print(struct.unpack('>f', data_order)[0])
+            elif (frame.cmd == b'\x48'):
+                print('Umidade: ')
+                print(struct.unpack('>f', data_order)[0])
